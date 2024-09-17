3166cef6cdd9a7c491d0faa4b8dd3c35b2520df7
day <laasiya.sivanandam@gmail.com>
2024-09-17 10:40:36 +0530

    new code added

--- src/test/java/Samp.java
+++ src/test/java/Samp.java
@@ -12,5 +12,17 @@ public class Samp {
 		System.out.println("method3");
 
 	}
+	private void method7() {
+		System.out.println("method7");
+
+	}
+	private void method8() {
+		System.out.println("method8");
+
+	}
+	private void method9() {
+		System.out.println("method9");
+
+	}
 
 }
